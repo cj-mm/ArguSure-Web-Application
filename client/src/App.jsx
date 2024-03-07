8a3f878cfae4be2471a81ca4ab009f65aacdcec7
cj-mm <cmmarasigan@up.edu.ph>
2024-03-07 23:05:39 +0800

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Liked from './pages/Liked'
+import Disliked from './pages/Disliked'
+import Saved from './pages/Saved'
+import History from './pages/History'
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/liked" element={<Liked />} />
+      <Route path="/disliked" element={<Disliked />} />
+      <Route path="/saved" element={<Saved />} />
+      <Route path="/history" element={<History />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
---
+++ client/src/pages/Disliked.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Disliked() {
+  return (
+    <div>Disliked</div>
+  )
+}
