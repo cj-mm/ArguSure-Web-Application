9f28b2ac263f06a8b2fefe5bc3c842e1474f68f8
cj-mm <cmmarasigan@up.edu.ph>
2024-03-29 01:48:30 +0800

    fix and style sidebar

--- client/src/components/AppSidebar.jsx
+++ client/src/components/AppSidebar.jsx
@@ -7,35 +7,44 @@ export default function AppSidebar() {
   const path = useLocation().pathname;
 
   return (
-    <Sidebar className="h-screen bg-cgreen">
+    <Sidebar className="h-screen app-sidebar w-60">
       <Sidebar.Logo href="/" img={AppLogo} imgAlt="App logo" as="div">
         <span className="text-clightgreen">Lorem Ipsum</span>
       </Sidebar.Logo>
+      <hr className="border-clightgreen mb-12" />
       <Sidebar.Items>
         <Sidebar.ItemGroup>
           <Link to="/">
             <Sidebar.Item active={path === "/"} as="div">
-              Home
+              <span className={path === "/" ? "text-clight" : ""}>HOME</span>
             </Sidebar.Item>
           </Link>
           <Link to="/liked">
             <Sidebar.Item active={path === "/liked"} as="div">
-              Liked
+              <span className={path === "/liked" ? "text-clight" : ""}>
+                LIKED
+              </span>
             </Sidebar.Item>
           </Link>
           <Link to="/disliked">
             <Sidebar.Item active={path === "/disliked"} as="div">
-              Disliked
+              <span className={path === "/disliked" ? "text-clight" : ""}>
+                DISLIKED
+              </span>
             </Sidebar.Item>
           </Link>
           <Link to="/saved">
             <Sidebar.Item active={path === "/saved"} as="div">
-              Saved
+              <span className={path === "/saved" ? "text-clight" : ""}>
+                SAVED
+              </span>
             </Sidebar.Item>
           </Link>
           <Link to="/history">
             <Sidebar.Item active={path === "/history"} as="div">
-              History
+              <span className={path === "/history" ? "text-clight" : ""}>
+                HISTORY
+              </span>
             </Sidebar.Item>
           </Link>
         </Sidebar.ItemGroup>
