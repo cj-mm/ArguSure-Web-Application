70ea098b74f32b39d064ad13f602006118746aec
cj-mm <cmmarasigan@up.edu.ph>
2024-04-03 21:21:32 +0800

    add like dislike api route

--- api/controllers/counterargument.controller.js
+++ api/controllers/counterargument.controller.js
@@ -28,3 +28,42 @@ export const saveCounterarg = async (req, res, next) => {
     next(error);
   }
 };
+
+export const likeDislike = async (req, res, next) => {
+  console.log(req.user);
+  if (!req.user) {
+    return next(errorHandler(403, "User not signed in"));
+  }
+
+  if (req.user.id !== req.body.userId) {
+    return next(errorHandler(403, "User not allowed to like/dislike"));
+  }
+
+  if (
+    req.body.liked !== "none" &&
+    req.body.liked !== "liked" &&
+    req.body.liked !== "disliked"
+  ) {
+    return next(errorHandler(403, "Invalid value"));
+  }
+
+  try {
+    const counterarg = await Counterargument.findById(req.body._id);
+    if (!counterarg) {
+      return next(errorHandler(404, "Counterargument not found"));
+    }
+
+    const updatedCounterarg = await Counterargument.findByIdAndUpdate(
+      req.body._id,
+      {
+        $set: {
+          liked: req.body.liked,
+        },
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedCounterarg);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/counterargument.route.js
+++ api/routes/counterargument.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { saveCounterarg } from "../controllers/counterargument.controller.js";
+import {
+  likeDislike,
+  saveCounterarg,
+} from "../controllers/counterargument.controller.js";
 
 const router = express.Router();
 
 router.post("/save", verifyToken, saveCounterarg);
+router.put("/like", verifyToken, likeDislike);
 
 export default router;
