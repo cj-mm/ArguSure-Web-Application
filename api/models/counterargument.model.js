897479ba9e73f50dc30b82c27d2987e3ffac3d4b
cj-mm <cmmarasigan@up.edu.ph>
2024-04-03 18:16:42 +0800

    create counterargument model

---
+++ api/models/counterargument.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const counterargumentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    inputClaim: {
+      type: String,
+      required: true,
+    },
+    summary: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    source: {
+      type: String,
+      required: true,
+    },
+    liked: {
+      type: String,
+      default: "none",
+      enum: ["none", "liked", "disliked"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Counterargument = mongoose.model(
+  "Counterargument",
+  counterargumentSchema
+);
+
+export default Counterargument;
