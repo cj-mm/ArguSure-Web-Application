d6e82f3bf95359ed5ba621281ea72e1d4450e331
cj-mm <cmmarasigan@up.edu.ph>
2024-04-12 14:33:22 +0800

    add backend get topics functionality

--- api/routes/saved.route.js
+++ api/routes/saved.route.js
@@ -6,6 +6,7 @@ import {
   renameTopic,
   deleteTopic,
   getSavedCounterargs,
+  getTopics,
 } from "../controllers/saved.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -15,5 +16,6 @@ router.put("/save", verifyToken, saveCounterargument);
 router.put("/unsave", verifyToken, unSaveCounterargument);
 router.put("/addtopic", verifyToken, addTopic);
 router.get("/getsaved", verifyToken, getSavedCounterargs);
+router.get("/gettopics", verifyToken, getTopics);
 
 export default router;
