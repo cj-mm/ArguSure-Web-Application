ea0677ce862b0fc4d67964502ab1739153fa72ad
cj-mm <cmmarasigan@up.edu.ph>
2024-04-20 16:06:43 +0800

    add get signed in user to the backend

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -3,8 +3,18 @@ import { checkUsername, checkPassword } from "../utils/validator.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 
-export const test = (req, res) => {
-  res.json({ message: "API is working!" });
+export const getSignedInUser = async (req, res) => {
+  if (!req.user) {
+    next(errorHandler(400, "No user signed in"));
+  }
+
+  try {
+    const currentUser = await User.findById(req.user.id);
+    const { password, ...rest } = currentUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const updateUser = async (req, res, next) => {
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,15 +1,15 @@
 import express from "express";
 import {
-  test,
   updateUser,
   deleteUser,
   signout,
+  getSignedInUser,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.get("/test", test);
+router.get("/getuser", verifyToken, getSignedInUser);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
