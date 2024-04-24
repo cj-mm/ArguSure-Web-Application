940fcd75e712f99a62d3ed2dfbe517203194f298
cj-mm <cmmarasigan@up.edu.ph>
2024-04-24 21:42:43 +0800

    make sidebar responsive

--- client/src/components/AppSidebar.jsx
+++ client/src/components/AppSidebar.jsx
@@ -3,15 +3,58 @@ import { Sidebar } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import AppLogo from "../assets/logo.png";
 import HeaderProfile from "./HeaderProfile";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 export default function AppSidebar() {
   const path = useLocation().pathname;
 
+  const handleSidebarMenu = () => {
+    const sidebar = document.getElementById("app-sidebar");
+    const sidebarclose = document.getElementById("sidebar-menu-close");
+    sidebar.style.display = "block";
+    sidebarclose.style.display = "block";
+    sidebar.style.boxShadow =
+      "0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0, 0, 0, 0.5)";
+  };
+
+  const handleSidebarMenuClose = () => {
+    const sidebar = document.getElementById("app-sidebar");
+    const sidebarclose = document.getElementById("sidebar-menu-close");
+    sidebar.style.display = "none";
+    sidebarclose.style.display = "none";
+    sidebar.style.boxShadow = "0";
+  };
+
+  window.addEventListener("resize", () => {
+    const sidebar = document.getElementById("app-sidebar");
+    const sidebarclose = document.getElementById("sidebar-menu-close");
+    if (window.innerWidth > 930) {
+      sidebar.style.display = "block";
+      sidebarclose.style.display = "none";
+      sidebar.style.boxShadow = "0";
+    } else {
+      sidebar.style.display = "none";
+      sidebarclose.style.display = "none";
+      sidebar.style.boxShadow = "0";
+    }
+  });
+
   return (
     <>
-      <AiOutlineMenu className="fixed" />
-      <Sidebar className="app-sidebar h-screen w-60 fixed">
+      <AiOutlineMenu
+        id="sidebar-menu"
+        className="sidebar-menu hidden fixed size-8 m-2 p-1 z-10 text-cbrown bg-clight rounded cshadow hover:cursor-pointer"
+        onClick={handleSidebarMenu}
+      />
+      <AiOutlineClose
+        id="sidebar-menu-close"
+        className="sidebar-menu-close fixed left-[215px] hidden size-8 m-2 p-1 z-20 text-cbrown bg-clight rounded cshadow hover:cursor-pointer"
+        onClick={handleSidebarMenuClose}
+      />
+      <Sidebar
+        id="app-sidebar"
+        className="app-sidebar h-screen w-60 fixed z-10"
+      >
         <Sidebar.Logo href="/" img={AppLogo} imgAlt="App logo" as="div">
           <span className="text-clightgreen">Lorem Ipsum</span>
         </Sidebar.Logo>
