0c8b7947aa27ded47539696d1a72372bb21f2de7
cj-mm <cmmarasigan@up.edu.ph>
2024-04-25 23:47:21 +0800

    add admin page dashboard ui

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -3,6 +3,7 @@ import { FaUsers } from "react-icons/fa";
 import { BsFillChatSquareTextFill } from "react-icons/bs";
 import { FaFolderOpen } from "react-icons/fa6";
 import { BiSolidLike, BiSolidDislike } from "react-icons/bi";
+import { PieChart } from "react-minimal-pie-chart";
 
 export default function () {
   return (
@@ -28,7 +29,7 @@ export default function () {
           </div>
           <div className="flex gap-1 h-20 bg-clight rounded cshadow p-3">
             <div className="flex flex-1 flex-col">
-              <span className="flex-1 text-sm">Topics Total</span>
+              <span className="flex-1 text-sm">Saved Total</span>
               <span className="text-2xl font-bold">169</span>
             </div>
             <FaFolderOpen className="size-12 text-cgreen m-auto" />
@@ -51,8 +52,47 @@ export default function () {
               <BiSolidDislike className="size-12 text-cgreen m-auto" />
             </div>
           </div>
-          <div className="h-40 sm:h-full bg-clightgreen rounded cshadow"></div>
-          <div className="h-40 sm:h-full bg-clightgreen rounded cshadow"></div>
+          <div className="h-40 sm:h-full bg-clight rounded cshadow p-3">
+            <div className="text-sm mb-2">Like to Dislike Ratio</div>
+            <div className="flex justify-center gap-5">
+              <div className="flex flex-col gap-1 h-full text-base font-bold my-auto">
+                <div className="flex gap-1">
+                  <div className="h-3 w-3 bg-clightgreen rounded my-auto"></div>
+                  <span>Likes: 75%</span>
+                </div>
+                <div className="flex gap-1">
+                  <div className="h-3 w-3 bg-cgreen rounded my-auto"></div>
+                  <span>Dislikes: 25%</span>
+                </div>
+              </div>
+              <PieChart
+                data={[
+                  { title: "Likes", value: 25, color: "#96E1D9" },
+                  { title: "Dislikes", value: 15, color: "#6EABB0" },
+                ]}
+                className="size-28"
+              />
+            </div>
+          </div>
+          <div className="h-full bg-clight rounded cshadow p-3">
+            <div className="text-sm mb-2">Past Month</div>
+            <div className="flex gap-1 text-sm justify-center">
+              <div className="flex flex-col border-2 border-cgreen p-2 rounded-l truncate overflow-x-auto">
+                <span>Added Users</span>
+                <span>Generated Counterarguments</span>
+                <span>Saved Counterarguments</span>
+                <span>Liked Counterarguments</span>
+                <span>Disliked Counterarguments</span>
+              </div>
+              <div className="flex flex-col font-bold border-2 border-cgreen p-2 rounded-r">
+                <span>169</span>
+                <span>169</span>
+                <span>169</span>
+                <span>169</span>
+                <span>169</span>
+              </div>
+            </div>
+          </div>
         </div>
         <div></div>
       </div>
