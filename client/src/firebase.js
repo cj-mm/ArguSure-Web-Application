68682a4d0fad6e5476e3230b75258bada9958101
cj-mm <cmmarasigan@up.edu.ph>
2026-04-21 00:52:16 +0800

    docs: remove unnecessary comments

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,9 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "special-problem-34788.firebaseapp.com",
@@ -13,5 +10,4 @@ const firebaseConfig = {
   appId: "1:369985327247:web:2a3d1ec6180c828d1e60fa",
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
